f0a228cc4239d589e9361e8bc4b7d5a4100fd49b
pat_k <z5125850@student.unsw.edu.au>
2020-04-06 23:01:35 +1000

    Add Bottom Navigation Bar + Fragments

--- app/src/main/java/com/example/a3634project/MainActivity.java
+++ app/src/main/java/com/example/a3634project/MainActivity.java
@@ -1,7 +1,15 @@
 package com.example.a3634project;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
+
 import android.os.Bundle;
+import android.view.MenuItem;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,8 +18,37 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        System.out.println("HI PATTTTTTT");
+        BottomNavigationView bottonNav = findViewById(R.id.bottom_navigation);
+        bottonNav.setOnNavigationItemSelectedListener(navListener);
+        getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, new HomeFragment()).commit();
     }
 
-    //Testing from Pat :)
+    private BottomNavigationView.OnNavigationItemSelectedListener navListener =
+            new BottomNavigationView.OnNavigationItemSelectedListener() {
+                @Override
+                public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+                    Fragment selectedFragment = null;
+                    FragmentManager manager = getSupportFragmentManager();
+                    FragmentTransaction transaction = manager.beginTransaction();
+                    switch(menuItem.getItemId()){
+                        case R.id.nav_home:
+                            selectedFragment = new HomeFragment();
+                            break;
+                        case R.id.nav_explore:
+                            selectedFragment = new ExploreFragment();
+                            break;
+                        case R.id.nav_diary:
+                            selectedFragment = new DiaryFragment();
+                            break;
+                        case R.id.nav_profile:
+                            selectedFragment = new ProfileFragment();
+                            break;
+                    }
+                    transaction.replace(R.id.fragment_container, selectedFragment).commit();
+
+                    return true;
+                }
+            };
+
+
 }

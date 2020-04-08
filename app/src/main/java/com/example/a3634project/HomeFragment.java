a6a15eec4e91a251148371d9f459669dfc44f2e1
pat_k <z5125850@student.unsw.edu.au>
2020-04-09 00:10:53 +1000

    Added RecyclerView to Home Fragment + Fixed orientation state issue

--- app/src/main/java/com/example/a3634project/HomeFragment.java
+++ app/src/main/java/com/example/a3634project/HomeFragment.java
@@ -2,27 +2,66 @@ package com.example.a3634project;
 
 import android.os.Bundle;
 
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
+import java.util.Calendar;
 
-/**
- * A simple {@link Fragment} subclass.
- */
 public class HomeFragment extends Fragment {
+    private RecyclerView mRecyclerView;
+    private RecyclerView.Adapter mAdapter;
+    private RecyclerView.LayoutManager mLayoutManager;
+    //private TextView greetings;
 
     public HomeFragment() {
         // Required empty public constructor
     }
 
-
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_home, container, false);
+        View v = inflater.inflate(R.layout.fragment_home, container, false);
+
+        mRecyclerView = v.findViewById(R.id.recyclerView);
+        mRecyclerView.setHasFixedSize(true);
+        mLayoutManager = new LinearLayoutManager(getActivity());
+        mRecyclerView.setLayoutManager(mLayoutManager);
+        TextView greetings = v.findViewById(R.id.greetingsTv);
+        greetings.setText(setGreetings());
+
+        TopicAdapter.RecyclerViewClickListener listener = new TopicAdapter.RecyclerViewClickListener() {
+            @Override
+            public void onClick(View view, int position) {
+
+            }
+        };
+
+        mAdapter = new TopicAdapter(Topic.getTopics(), listener);
+        mRecyclerView.setAdapter(mAdapter);
+
+        return v;
+    }
+
+    public String setGreetings(){
+        Calendar c = Calendar.getInstance();
+        int timeOfDay = c.get(Calendar.HOUR_OF_DAY);
+        String greetings = "";
+        if(timeOfDay >= 0 && timeOfDay < 12){
+            greetings = "Good Morning";
+        } else if(timeOfDay >= 12 && timeOfDay < 16){
+            greetings = "Good Afternoon";
+        } else if(timeOfDay >= 16 && timeOfDay < 24){
+            greetings = "Good Evening";
+        }
+        return greetings;
     }
 }
+
